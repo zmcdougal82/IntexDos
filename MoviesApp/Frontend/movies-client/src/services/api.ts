da33f638f1372f39ccb11c7a53de3a1b85a75340
zmcdougal82 <zacktmcdougal@gmail.com>
2025-04-09 14:35:26 -0600

    please

--- MoviesApp/Frontend/movies-client/src/services/api.ts
+++ MoviesApp/Frontend/movies-client/src/services/api.ts
@@ -18,6 +18,7 @@ const api = axios.create({
   headers: {
     "Content-Type": "application/json",
   },
+  withCredentials: true, // Enables sending cookies with cross-origin requests
 });
 
 // Add a request interceptor to include the JWT token in requests
